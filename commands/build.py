44f4c4c61a7b21c2e082e04f19cde5c5ed616a41
Stephen Gaito <stephen@perceptisys.co.uk>
2022-03-02 15:16:53 +0000

    updated build command

--- commands/build.py
+++ commands/build.py
@@ -1,17 +1,26 @@
 # This file contains the build commands.
 
 import click
+import os
+import platform
 import yaml
 
 from cpcli.utils import runCommandWithNatsServer
 
-async def sillyCmd(config, natsServer) :
-  print("Hello from sillyCmd!")
-  await natsServer.sendMessage("SillySubject", "aMessage")
+async def sendBuildConTeXtCmd(config, natsServer) :
+  print("Building a ConTeXt document")
+  buildData = {
+    'projectDir'    : 'sillyDir',
+    'taskName'      : 'a silly task',
+    'doc'           : 'silliestDoc.tex',
+    'rsyncHostName' : platform.node(),
+    'rsyncUserName' : os.getlogin()
+  }
+  await natsServer.sendMessage("build.from.context", buildData)
 
 @click.command(short_help="build a ConTeXt document.")
 def build() :
   print("Hello from builder")
-  runCommandWithNatsServer(sillyCmd)
+  runCommandWithNatsServer(sendBuildConTeXtCmd)
   print("Bye from builder")
 
